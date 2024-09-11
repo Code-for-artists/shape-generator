d4081d80af72ec0c92946a17b0135ca6d79333b8
erifranck <erifranck.nunez-nd@disney.com>
2024-09-11 13:15:33 -0500

    Fix(-): responsive size

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 import { Shape } from './components/Shape';
 import { randomIntFromInterval } from "./utils/shapeGenerators";
@@ -8,25 +9,27 @@ import { downloadSVGAsPNG } from './utils/downloadPng';
 
 function App() {
   const [edges, setEdges] = useState(randomIntFromInterval(6, 20))
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
   const svgRef = useRef(null)
   const handleRandomEdges = () => {
     const newEdges = randomIntFromInterval(6, 20)
     setEdges(newEdges)
   }
   const handleDownload = () => downloadSVGAsPNG(svgRef.current)
+  const size = isSmallDevice ? 350 : 600
 
   return (
     <div className="App">
       <div>
         <div className="button-container">
           <button class="edges-button" onClick={handleRandomEdges}>
-            change edges
+            Actualizar bordes
           </button>
           <button class="download-button" onClick={handleDownload}>
-            download image 
+            Descargar
           </button>
         </div>
-        <Shape svgRef={svgRef} size={600} edges={edges} />
+        <Shape svgRef={svgRef} size={size} edges={edges} />
       </div>
     </div>
   );
