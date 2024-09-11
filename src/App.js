cecb77230420ea733ed60e381cb9c79f3c24ac75
erifranck <erifranck.nunez-nd@disney.com>
2024-09-11 12:43:50 -0500

    Fix(-): add react import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { Shape } from './components/Shape';
 import { randomIntFromInterval } from "./utils/shapeGenerators";
